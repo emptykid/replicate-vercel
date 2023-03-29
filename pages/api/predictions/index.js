ec769b000b5f6cf6aee091620a932c94bccfca61
xiaoqiang <qiang0902@126.com>
2023-03-29 20:33:45 +0800

    update to openjourney

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -14,7 +14,8 @@ export default async function handler(req, res) {
   const prediction = await replicate.predictions.create({
     // Pinned to a specific version of Stable Diffusion
     // See https://replicate.com/stability-ai/stable-diffussion/versions
-    version: "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    //version: "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    version: "9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb",
 
     // This is the text prompt that will be submitted by a form on the frontend
     input: { prompt: req.body.prompt },
